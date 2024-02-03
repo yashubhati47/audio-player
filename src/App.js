b8cf6c8575ea59f44547499295c6111c82e54288
yashubhati47 <yashpalbhati47@gmail.com>
2024-02-03 13:31:00 +0530

    commit now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState, useRef, useEffect } from 'react';
 
-function App() {
+const AudioPlayer = () => {
+  const [playlist, setPlaylist] = useState([]);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const storedPlaylist = JSON.parse(localStorage.getItem('playlist')) || [];
+    const storedTrackIndex = parseInt(localStorage.getItem('currentTrackIndex')) || 0;
+    const storedCurrentTime = parseFloat(localStorage.getItem('currentTime')) || 0;
+
+    setPlaylist(storedPlaylist);
+    setCurrentTrackIndex(storedTrackIndex);
+    setCurrentTime(storedCurrentTime);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('playlist', JSON.stringify(playlist));
+    localStorage.setItem('currentTrackIndex', currentTrackIndex);
+    localStorage.setItem('currentTime', currentTime);
+  }, [playlist, currentTrackIndex, currentTime]);
+
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+
+    if (files.length > 0) {
+      const newPlaylist = [...playlist];
+
+      for (const file of files) {
+        newPlaylist.push({
+          name: file.name,
+          url: URL.createObjectURL(file),
+        });
+      }
+
+      setPlaylist(newPlaylist);
+    }
+  };
+
+  const playTrack = (index) => {
+    setCurrentTrackIndex(index);
+    audioRef.current.currentTime = currentTime;
+    audioRef.current.play();
+  };
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audioRef.current.currentTime);
+  };
+
+  const playNextTrack = () => {
+    setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % playlist.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" accept=".mp3" onChange={handleFileChange} multiple />
+      <audio
+        ref={audioRef}
+        src={playlist[currentTrackIndex]?.url}
+        controls
+        autoPlay
+        onEnded={playNextTrack}
+        onTimeUpdate={handleTimeUpdate}
+        currentTime={currentTime}
+      />
+      <h2>Playlist</h2>
+      <ul>
+        {playlist.map((track, index) => (
+          <li key={index} onClick={() => playTrack(index)}>
+            {track.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default AudioPlayer;
